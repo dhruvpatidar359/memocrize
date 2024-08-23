c51658260df683605eb6a092f273bfa8a06635aa
Dhruv Patidar <103873587+dhruvpatidar359@users.noreply.github.com>
2024-08-23 17:40:09 +0530

    feat: mobile app

---
+++ memocrize-app/memotips/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:memotips/screens/home.dart';
+import 'package:memotips/screens/qr_scan.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "SpaceGrotesk",
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
